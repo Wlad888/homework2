3d6cf64971d8b3ccb3a853e2c7a189381471aeb0
Wlad888 <vladesanu@gmail.com>
2022-08-01 16:36:53 +0300

    HoweWork2 (2tasks)

---
+++ homework2/src/HomeWork2.java
@@ -0,0 +1,40 @@
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+        int a = 55;
+        int b = 55;
+        if (a < b) {
+            System.out.println("a < b");
+        }else if (a > b) {
+            System.out.println(" a > b");
+        }
+        else {
+            System.out.println("a = b");
+        }
+
+        int x = 5;
+        switch (x) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("This is a wrong number");
+        }
+    }
+
+}
+
+
+
